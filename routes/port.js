dd5152262d6c533b1bc74f283a04710bfe6ba82c
guodongdong <1486175219@qq.com>
2017-03-01 10:07:51 +0800

    3.1

--- routes/port.js
+++ routes/port.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var PostModel = require('../models/posts');
+var CommentModel = require('../models/comments');
 var SerialPort = require('serialport');  //引入模块
 var port = new SerialPort('com6');
+
 // port.open(function(error){
 //    if(error){
 //      console.log(error);
@@ -45,7 +48,29 @@ var datapool1=function(){
 ////module.exports = function (app) {
 //    //var port = require('./port')
 router.get('/', function(req, res, next) {
-    res.render('port',{msg:datapool1()});
+    var keyword = datapool1();
+
+    Promise.all([
+        //PostModel.getPostById(keyword),// 获取文章信息
+        PostModel.search(keyword),
+        CommentModel.getComments(keyword),// 获取该文章所有留言
+        //PostModel.search(keyword),
+        PostModel.incPv(keyword)// pv 加 1
+    ])
+        .then(function (result) {
+            var post = result[0];
+            var comments = result[1];
+            if (!post) {
+                throw new Error('该信息不存在');
+            }
+
+            res.render('search', {
+                post: post,
+                comments: comments
+            });
+        })
+        .catch(next);
+    // res.render('port',{postId:datapool1()});
 });
 //router.get('/', function (req, res) {
 //    res.render('port' ,{msg:'datapool1'})
--- routes/search.js
+++ routes/search.js
@@ -4,31 +4,7 @@ var PostModel = require('../models/posts');
 var CommentModel = require('../models/comments');
 
 var checkLogin = require('../middlewares/check').checkLogin;
-//router.get('/', function (req, res) {
-//    PostModel.search(req.query.keyword, function (err, posts) {
-//        if (err) {
-//            req.flash('error', err);
-//            return res.redirect('/');
-//        }
-//        res.render('search', {
-//            posts: posts
-//        });
-//    });
-//});
-//
-//router.get('/', function(req, res) {
-//    var keyword = req.query.keyword;
-//
-//    PostModel.search(keyword)
-//        .then(function (posts) {
-//            res.render('search', {
-//                posts: posts
-//            });
-//        })
-//        .catch(next);
-//});
 
-//// GET /posts/:postId 单独一篇的文章页
 router.get('/', function(req, res, next) {
     //var postId = req.params.postId;
     var keyword = req.query.postId;
