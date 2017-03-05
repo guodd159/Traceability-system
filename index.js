fd424fc40dd01190115e983ea9765419dd22248b
guodongdong <1486175219@qq.com>
2017-03-05 16:49:49 +0800

    日志打印

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var flash = require('connect-flash');
 var config = require('config-lite');
 var routes = require('./routes');
 var pkg = require('./package');
-
+var winston = require('winston');
+var expressWinston = require('express-winston');
 var app = express();
 
 // 设置模板目录
@@ -20,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
   secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  resave: true,// 强制更新 session
+  saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
   cookie: {
     maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
   },
@@ -49,10 +52,32 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error').toString();
   next();
 });
-
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new (winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'
+        })
+    ]
+}));
 // 路由
 routes(app);
-
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'
+        })
+    ]
+}));
 // error page
 app.use(function (err, req, res, next) {
   res.render('error', {
