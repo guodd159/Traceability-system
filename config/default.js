8eb2f684c179991afba6b55be5dff555903625c5
guodongdong <1486175219@qq.com>
2017-03-18 16:21:18 +0800

    配置环境变量

--- config/default.js
+++ config/default.js
@@ -1,9 +1,3 @@
 module.exports = {
-  port: 1000,
-  session: {
-    secret: 'traceability',
-    key: 'traceability',
-    maxAge: 2592000000
-  },
-  mongodb: 'mongodb://127.0.0.1:27017/traceability'
+
 };
